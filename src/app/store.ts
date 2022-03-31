7b3c24549789b166614733cbe9e07aaf837d0e58
mskuratova <m.p.skuratova@gmail.com>
2022-03-31 18:02:01 +0600

    22 lesson and saga

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,14 +1,26 @@
 import thunkMiddleware from 'redux-thunk'
 import {configureStore} from '@reduxjs/toolkit'
 import {rootReducer} from './reducers'
+import createSagaMiddleware from 'redux-saga'
+import {takeEvery} from 'redux-saga/effects'
 
 
+const sagaMiddleware = createSagaMiddleware()
 
 export const store = configureStore({
     reducer: rootReducer,
-    middleware: getDefaultMiddleware => getDefaultMiddleware().prepend(thunkMiddleware)
+    middleware: getDefaultMiddleware => getDefaultMiddleware().prepend(thunkMiddleware, sagaMiddleware)
 })
 
+sagaMiddleware.run(rootWatcher)
+
+function* rootWatcher() {
+    yield takeEvery("ACCTIVATOR-ACTION-TYPE", rootWorker)
+}
+
+function* rootWorker() {
+
+}
 // а это, чтобы можно было в консоли браузера обращаться к store в любой момент
 // @ts-ignore
 window.store = store
