44d246da1740beb4eee8ea665c23ce14dd53e344
mskuratova <m.p.skuratova@gmail.com>
2022-04-15 19:29:38 +0600

    22.2++ lesson and saga

--- src/app/reducers.ts
+++ src/app/reducers.ts
@@ -3,13 +3,10 @@ import {combineReducers} from 'redux'
 import {appReducer} from '../features/Application'
 import {authReducer} from '../features/Auth'
 import {tasksReducer, todolistsReducer} from '../features/TodolistsList'
-import {call, put} from 'redux-saga/effects';
-import {takeEvery} from 'redux-saga/effects'
+import {call, put} from 'redux-saga/effects'
 import {setIsLoggedIn} from "../features/Auth/auth-reducer";
 import {authAPI} from "../api/todolists-api";
-// объединяя reducer-ы с помощью combineReducers,
-// непосредственно создаём store
-//export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+
 export const rootReducer = combineReducers({
     app: appReducer,
     auth: authReducer,
