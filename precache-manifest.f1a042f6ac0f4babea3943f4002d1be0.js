a71c8bfa47e9467af3ebef00e71346699a59fc51
mskuratova <m.p.skuratova@gmail.com>
2022-04-20 18:55:22 +0600

    Updates

---
+++ precache-manifest.f1a042f6ac0f4babea3943f4002d1be0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98ab2b24bbdc9f0ef0f3d627f22338fb",
+    "url": "/todolist-ts-09/index.html"
+  },
+  {
+    "revision": "a42c7e40586f05aae84a",
+    "url": "/todolist-ts-09/static/css/main.155097fa.chunk.css"
+  },
+  {
+    "revision": "74b26f97c108942f9a97",
+    "url": "/todolist-ts-09/static/js/2.abf1ffd7.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/todolist-ts-09/static/js/2.abf1ffd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a42c7e40586f05aae84a",
+    "url": "/todolist-ts-09/static/js/main.9643df72.chunk.js"
+  },
+  {
+    "revision": "672ef206d5517191e14f",
+    "url": "/todolist-ts-09/static/js/runtime-main.e9d2bd64.js"
+  }
+]);
